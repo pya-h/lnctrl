fc5e179bf94049e829e87a08c6a73aa25e97c811
pya-py <p4ya.py@gmail.com>
2022-08-02 02:59:29 +0430

    in Bode: gain slider added and updated; in root-locus: poles and zeros are now shown in the plot

--- src/store/index.js
+++ src/store/index.js
@@ -6,11 +6,11 @@ const persister = "Free";
 
 const store = createStore(
     reducers,
-    compose(
+    // compose(
         applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__() //temp: enable chrome redux manager extension
-    )
+    //     window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    //         window.__REDUX_DEVTOOLS_EXTENSION__() //temp: enable chrome redux manager extension
+    // )
 );
 
 export { store, persister };

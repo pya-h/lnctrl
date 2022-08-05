20a7c896a8020cf7f91e936de1bdfef9691d5621
pya-py <p4ya.py@gmail.com>
2022-08-06 00:05:03 +0430

    laplace algorythom completing; deriavative method added for transfer functions; constructing tf from poles and zeros list;

--- src/App.js
+++ src/App.js
@@ -12,8 +12,10 @@ import themes from "themes";
 // project imports
 import NavigationScroll from "layout/NavigationScroll";
 import { MathJaxContext } from "better-react-mathjax";
+
 const App = () => {
     const customization = useSelector((state) => state.customization);
+
     return (
         <StyledEngineProvider injectFirst>
             <ThemeProvider theme={themes(customization)}>

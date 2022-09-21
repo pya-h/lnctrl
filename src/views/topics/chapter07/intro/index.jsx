63285bdd76b3136e8982ab32bb58e16773064712
pya-py <p4ya.py@gmail.com>
2022-09-21 09:39:36 +0430

    intros are done

--- src/views/topics/chapter07/intro/index.jsx
+++ src/views/topics/chapter07/intro/index.jsx
@@ -1,15 +1,36 @@
-import SubCard from "../../../ui-component/cards/SubCard";
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import "views/topics/topics.css";
+import PinchPanCard from "views/ui-component/cards/PinchPanCard";
+import img_temp1 from "./img/temp1.JPG";
+import img_temp2 from "./img/temp2.JPG";
+import img_temp3 from "./img/temp3.JPG";
 
 const Ch07Intro = () => {
-  return (
-    <SubCard title="مقدمه" darkBorder={true} sx={{ direction: "rtl" }}>
-      <Typography>
-        <h1>انواع کنترل کننده ها</h1>
-      </Typography>
-    </SubCard>
-  );
+    return (
+        <PinchPanCard title="مقدمه" border={true}>
+            <Grid xs={12} sx={{ m: "auto", textAlign: "center" }}>
+                <img
+                    style={{ maxWidth: "60rem", height: "auto" }}
+                    src={img_temp1}
+                    alt="بارگذاری تصویر با مشکل مواجه شد"
+                />
+            </Grid>
+            <Grid xs={12} sx={{ m: "auto", textAlign: "center" }}>
+                <img
+                    style={{ maxWidth: "62rem", height: "auto" }}
+                    src={img_temp2}
+                    alt="بارگذاری تصویر با مشکل مواجه شد"
+                />
+            </Grid>
+            <Grid xs={12} sx={{ m: "auto", textAlign: "center" }}>
+                <img
+                    style={{ maxWidth: "62rem", height: "auto" }}
+                    src={img_temp3}
+                    alt="بارگذاری تصویر با مشکل مواجه شد"
+                />
+            </Grid>
+        </PinchPanCard>
+    );
 };
 
 export default Ch07Intro;

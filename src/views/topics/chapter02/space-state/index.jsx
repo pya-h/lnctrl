7a6860da37c4ada04071631c4c8ded643554c71d
pya-h <p4ya.py@gmail.com>
2024-01-03 00:45:02 +0330

    state space exampe caching implemented

--- src/views/topics/chapter02/space-state/index.jsx
+++ src/views/topics/chapter02/space-state/index.jsx
@@ -1,62 +1,69 @@
-
 import SpaceStateLecture from "./lecture";
-
-import { useState } from "react";
 import SpaceStateParameters from "./parameters";
 import { Grid } from "@mui/material";
-import SpaceStateEquationSolveBox from './solve';
+import SpaceStateEquationSolveBox from "./solve";
+import TopicBaseComponent from "views/topics/TopicBaseComponent";
 
-const SpaceStateExample = () => {
-    const [A, $A] = useState([
-        [0, 1],
-        [-3, -4],
-    ]);
+export default class SpaceStateExample extends TopicBaseComponent {
+	state = {
+		A: [
+			[0, 1],
+			[-3, -4],
+		],
+		C: [1, 0],
+		x_0: [[1], [-1]],
+		topicKey: "ch2-ss",
+	};
 
-    const [C, $C] = useState([1, 0]);
+	$A = (matrixA) => this.updateState("A", matrixA);
 
-    const [x_0, $x_0] = useState([[1], [-1]]);
-    return (
-        <Grid container direction="column" spacing={1}>
-            <Grid
-                style={{
-                    width: "100%",
-                    height: "100%",
-                    margin: "auto",
-                    direction: "ltr",
-                }}
-                item
-            >
-                <SpaceStateLecture />
-            </Grid>
+	$C = (vectorC) => this.updateState("C", vectorC);
 
-            <Grid
-                spacing={2}
-                style={{
-                    width: "100%",
-                    height: "100%",
-                    margin: "auto",
-                    marginTop: "1%",
-                    direction: "ltr",
-                }}
-                container
-            >
-                <Grid lg={4} md={12} sm={12} xs={12} item>
-                    <SpaceStateParameters
-                       A={A}
-                       $A={$A}
-                       C={C}
-                       $C={$C}
-                       x_0={x_0}
-                       $x_0={$x_0}
+	$x_0 = (xInitials) => this.updateState("x_0", xInitials);
 
-                    />
-                </Grid>
-                <Grid lg={8} md={12} sm={12} xs={12} item>
-                    <SpaceStateEquationSolveBox A={A} C={C} x_0={x_0} />
-                </Grid>
-            </Grid>
-        </Grid>
-    );
-};
+	render() {
+		const { A, C, x_0 } = this.state;
 
-export default SpaceStateExample;
+		return (
+			<Grid container direction="column" spacing={1}>
+				<Grid
+					style={{
+						width: "100%",
+						height: "100%",
+						margin: "auto",
+						direction: "ltr",
+					}}
+					item
+				>
+					<SpaceStateLecture />
+				</Grid>
+
+				<Grid
+					spacing={2}
+					style={{
+						width: "100%",
+						height: "100%",
+						margin: "auto",
+						marginTop: "1%",
+						direction: "ltr",
+					}}
+					container
+				>
+					<Grid lg={4} md={12} sm={12} xs={12} item>
+						<SpaceStateParameters
+							A={A}
+							$A={this.$A}
+							C={C}
+							$C={this.$C}
+							x_0={x_0}
+							$x_0={this.$x_0}
+						/>
+					</Grid>
+					<Grid lg={8} md={12} sm={12} xs={12} item>
+						<SpaceStateEquationSolveBox A={A} C={C} x_0={x_0} />
+					</Grid>
+				</Grid>
+			</Grid>
+		);
+	}
+}
